ef57430d39d4c3253f9524657c9bd5c49780c21b
harukoya <ka.uni.stu@gmail.com>
2022-06-04 18:58:47 +0900

    devise_token_authでUserを扱えるように

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :test, only: %i[index]
+
+      mount_devise_token_auth_for 'User', at: 'auth'
+    end
+  end
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
