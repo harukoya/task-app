c73f2096598fe549c5ded3b0aa025e643c3cba3a
harukoya <ka.uni.stu@gmail.com>
2022-07-31 20:08:07 +0900

    backend. action_controllerのwrap_paramをなくして、ユーザ登録できるように

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::API
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name])
   end
+
 end
