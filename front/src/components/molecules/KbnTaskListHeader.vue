6cd06a833252df9f1b9f1cc99a2e84d099d81ea8
harukoya <ka.uni.stu@gmail.com>
2022-08-09 09:35:20 +0900

    add taskListHeader

---
+++ front/src/components/molecules/KbnTaskListHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="title">
+      <h2>{{ taskListHeaderName }}</h2>
+    </div>
+    <div class="plusIcon">
+      <KbnIcon 
+        @click="$emit('add')"
+        icon="fa-plus"
+      ></KbnIcon>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+import KbnIcon from '@/components/atoms/KbnIcon.vue'
+
+export default {
+  components: {
+    KbnIcon,
+  },
+  props: {
+    taskListHeaderName: {
+      type: String,
+      required: true
+    }
+  },
+
+}
+</script>
